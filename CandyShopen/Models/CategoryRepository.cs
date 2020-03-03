c5f2f486ecd3ec07c9d28eff8996f2c1941a464c
Lars Lundin <larslundin72@live.se>
2020-03-03 18:02:43 +0100

    Add project files.

---
+++ CandyShopen/Models/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShopen.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> GetAllCategories => new List<Category>
+        {
+            new Category{CategoryId=1, CategoryName="Hard Candy", CategoryDescription="Awesome and delicious hard candy"},
+            new Category{CategoryId=2, CategoryName="Chocolate Candy", CategoryDescription="Scuptious Chocolate Candy"},
+            new Category{CategoryId=3, CategoryName="Fruit Candy", CategoryDescription="Sweet and sour Fruit Candy"}
+        };
+    }
+}
---
+++ CandyShopen/Models/ICandyRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShopen.Models
+{
+    public interface ICandyRepository
+    {
+        IEnumerable<Candy> GetAllCandy { get; set; }
+        IEnumerable<Candy> GetCandyOnSale { get; set; }
+
+        Candy GetCandyById(int candyId);
+
+    }
+}
