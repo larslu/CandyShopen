c77f1631d37afa7aaa49c8943d7ff3e4edead2e8
Lars Lundin <larslundin72@live.se>
2020-05-08 12:35:39 +0200

    La till viewcomponents

---
+++ CandyShopen/Components/CategoryMenu.cs
@@ -0,0 +1,14 @@
+﻿using CandyShopen.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShopen.Components 
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+    }
+}
---
+++ CandyShopen/Components/ShoppingCartSummary.cs
@@ -0,0 +1,33 @@
+﻿using CandyShopen.Models;
+using CandyShopen.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShopen.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart cart)
+        {
+            _shoppingCart = cart;
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            _shoppingCart.ShoppingCartItems = _shoppingCart.GetShoppingCartItems(); 
+
+            ShoppingCartViewModel shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+
+        }
+    }
+}
